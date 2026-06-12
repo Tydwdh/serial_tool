31280948bf6fcab7f31a69868e3e48120dad1773
tyd <2756433956@qq.com>
2026-06-13 03:46:29 +0800

    feat(lua): improve template.hello to demonstrate hw.utils and hw.codec

--- plugins/template.hello/main.lua
+++ plugins/template.hello/main.lua
@@ -1,18 +1,24 @@
--- Hello 插件模板
+-- Hello 插件模板 (v0.2)
 --
 -- 功能：
 --   1. 输出日志
---   2. 创建一个动态表单
---   3. 监听表单变更
---   4. 停用时清理面板
+--   2. 创建动态表单（text/select/checkbox）
+--   3. 监听表单变更并持久化
+--   4. 演示 hw.utils 和 hw.codec
+--   5. 停用时清理面板
 --
 -- 复制本目录开发新插件时，请修改：
 --   plugin.json 的 id/name/version
 --   面板 id，避免和其他插件冲突
 
+local u = require("hw.utils")
+local c = require("hw.codec")
+
 local PANEL_ID = "template.hello.form"
 
 ctx.log.info("Hello 插件已启动: " .. tostring(ctx.plugin.id))
+ctx.log.info("hw.utils demo: format_size(4096) = " .. u.format_size(4096))
+ctx.log.info("hw.codec demo: to_hex('AB') = " .. c.to_hex("AB"))
 
 ctx.ui.create_form({
     id = PANEL_ID,
